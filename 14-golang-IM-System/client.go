4fb0c7bf3d8925c09bc202369910ed5dc355cb5b
zimingming1996 <zimingming1996@foxmail.com>
2021-12-13 23:20:54 +0800

    12. 私聊模式

--- 14-golang-IM-System/client.go
+++ 14-golang-IM-System/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -44,11 +46,88 @@ func (client *Client) menu() bool {
 	fmt.Scanln(&flag)
 
 	if flag >= 0 && flag <= 3 {
-
+		client.flag = flag
 		return true
 	} else {
 		fmt.Println(">>> 请输入合法范围的数字 <<<")
 		return false
+	}
+}
+
+func (client *Client) PublicChat() {
+	//
+	var chatMsg string
+	fmt.Println(">>> 请输入聊天内容， exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "exit" {
+		sendMSg := chatMsg + "\n"
+		_, err := client.conn.Write([]byte(sendMSg))
+		if err != nil {
+			fmt.Println("conn Write err:", err)
+			break
+		}
+
+		chatMsg = ""
+		fmt.Println(">>> 请输入聊天内容， exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+
+}
+
+func (client *Client) UpdateName() bool {
+
+	fmt.Println(">>> 请输入用户名： ")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+	return true
+}
+
+//查询在新用户
+func (client *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn Write err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+	client.SelectUsers()
+	fmt.Println(">>> 请输入聊天对象【用户名】, exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println(">>> 请输入消息内容， exit退出")
+		fmt.Scanln(chatMsg)
+
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMSg := "to|" + remoteName + "|" + chatMsg + "\n"
+				_, err := client.conn.Write([]byte(sendMSg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+				chatMsg = ""
+				fmt.Println(">>> 请输入聊天内容， exit退出")
+				fmt.Scanln(&chatMsg)
+			}
+
+		}
+		client.SelectUsers()
+		fmt.Println(">>> 请输入聊天对象【用户名】, exit退出")
+		fmt.Scanln(&remoteName)
 	}
 }
 
@@ -62,15 +141,18 @@ func (client *Client) Run() {
 		case 1:
 			//
 			fmt.Println("选择公聊模式")
+			client.PublicChat()
 			break
 
 		case 2:
 			//
 			fmt.Println("选择私聊模式")
+			client.PrivateChat()
 			break
 		case 3:
 			//
 			fmt.Println("更新用户名")
+			client.UpdateName()
 			break
 		case 0:
 			//
@@ -79,6 +161,18 @@ func (client *Client) Run() {
 
 		}
 	}
+}
+
+func (client *Client) DealResponse() {
+
+	// 一旦client.conn有数据， 就直接copy到stdout标准输出上
+	io.Copy(os.Stdout, client.conn)
+
+	// for {
+	// 	buf := make([]byte, 4096)
+	// 	client.conn.Read(buf)
+	// 	fmt.Println(buf)
+	// }
 }
 
 var serverIp string
@@ -97,6 +191,9 @@ func main() {
 		fmt.Println(" >>> 链接服务器失败...")
 		return
 	}
+
+	go client.DealResponse()
+
 	fmt.Println(" >>> 链接服务器成功...")
 	client.Run()
 }
